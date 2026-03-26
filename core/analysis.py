0658b0f3ef11b77f8575ab858a68d97f70cdd6c9
wellhell42-ops <wellhell42@gmail.com>
2026-03-26 18:01:11 +0300

    Add functions for world race analysis and tempo calculations

---
+++ core/analysis.py
@@ -0,0 +1,9 @@
+def analiz_kosucu_atlar():
+    """Function to analyze horse racing data in the world race context."""
+    # Implementation goes here
+    pass
+
+def karsilastir_dunya_yerli():
+    """Function to compare world and local races."""
+    # Implementation goes here
+    pass
